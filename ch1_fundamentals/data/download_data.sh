ed5cb2e20892ed790a0733e1b4f78abd346e969d
Tim Gasser <timgasser@hotmail.com>
2016-11-30 17:13:52 +0000

    Added download script to get Union Find datasets

---
+++ ch1_fundamentals/data/download_data.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+echo Downloading Union-find data ..
+
+wget http://algs4.cs.princeton.edu/15uf/tinyUF.txt
+wget http://algs4.cs.princeton.edu/15uf/mediumUF.txt
+wget http://algs4.cs.princeton.edu/15uf/largeUF.txt
+
