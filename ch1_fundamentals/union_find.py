b8799c5e373f538a18c35dfbcc0744a44b94ecfe
Tim Gasser <timgasser@hotmail.com>
2016-11-30 22:36:05 +0000

    Added Quick-find implementation, running on tinyUF data

--- ch1_fundamentals/union_find.py
+++ ch1_fundamentals/union_find.py
@@ -7,7 +7,7 @@ import timeit
 
 # Constants
 DATA_DIR = 'data'
-
+DEF_FILE = DATA_DIR + '/tinyUF.txt'
 
 class UnionFind(object):
     ''' Abstract type for Union Find applications '''
@@ -55,9 +55,6 @@ class NaiveUnionFind(UnionFind):
     Uses a 2-level list structure for components -> sites
     '''
     def __init__(self, N):
-        ''' Pseudocode
-        - Create a list of N sets, all initially empty
-        '''
         # print('Creating Union Find with {} entries'.format(N))
         self.components = list()
 
@@ -65,17 +62,9 @@ class NaiveUnionFind(UnionFind):
         rep = 'Printing Union Find with {} components:\n'.format(len(self.components))
         for idx, comp in enumerate(self.components):
             rep += 'Component: {}\n'.format(comp)
-            # for site in comp:
-            #     rep += ' -> {}\n'.format(site)
         return rep
 
     def union(self, p, q):
-        ''' Pseudocode
-        - Find component `a` containing site `p`, and `b` with site `q`
-        - If a == b then early out
-        - Combine component b to component a (set union)
-        - Remove component b from the list of components
-        '''
         # print('Union of sites {} and {}'.format(p, q))
         comp_p = self.find(p)
         comp_q = self.find(q)
@@ -99,7 +88,7 @@ class NaiveUnionFind(UnionFind):
         if comp_q is None: # comp_p is not None, add q to p's component
             self.components[comp_p_idx].append(q)
             # print('Adding {} to {}\'s component -> {}'.format(q, p, self.components[comp_p_idx]))
-            return
+            return 
 
         if comp_p == comp_q:
             # print('Skipping - {} and {} already in same component {}'.format(p, q, comp_p))
@@ -112,12 +101,6 @@ class NaiveUnionFind(UnionFind):
         return
 
     def find(self, p):
-        ''' Pseudocode
-        - For each component in component list:
-          - If component set contains p, return component
-          
-        - Return error (site not found)
-        '''
         # print('Find of site {}'.format(p))
 
         for comp in self.components:
@@ -129,11 +112,6 @@ class NaiveUnionFind(UnionFind):
         return None
 
     def connected(self, p, q):
-        ''' Pseudocode
-        - If find (p) == find(q)
-            - return True
-        return False
-        '''
         # print('Connected for sites {} and {}'.format(p, q))
         if self.find(p) == self.find(q):
             return True
@@ -141,18 +119,167 @@ class NaiveUnionFind(UnionFind):
         return False
 
     def count(self):
-        ''' Pseudocode
-        return length of Component list
-        ''' 
         # print('Count called')
         return len(self.components)
 
 
+class BookUnionFind(UnionFind):
+    ''' Superclass with common functionality of all Union Find algos in book'''
+    def __init__(self, N):
+        '''
+        Initializes Union Find structure with N entries
+        '''
+        # The site number is implicitly stored as the self.id index
+        print('Super init: N = {}'.format(N))
+        self.N = N # Call this N to avoid namespace clash with count function
+        self.id = list(range(N))
+        
+        print('Super init: N = {}, id = {}'.format(self.N, self.id))
+        
+    def union(self, p, q):
+        '''
+        Merge components if two sites are in different components 
+          -> Implement this in subclass
+        INPUT: Site identifiers p and q
+        RETURNS: None
+        '''
+        raise NotImplementedError()
+        
+    def find(self, p):
+        '''
+        Component identifier for p
+          -> Implement this in subclass
+        INPUT: Site p
+        RETURNS: Component ID
+        '''
+        raise NotImplementedError()
+    
+    def connected(self, p, q):
+        '''
+        Returns True if p and q are in the same component
+        INPUT: Nodes p and q
+        RETURNS: Boolean showing if the nodes are connected
+        '''
+        print('Super connected for {} and {}'.format(p, q))
+        return self.find(p) == self.find(q)
+
+    def count(self):
+        '''
+        Returns number of components (not sites)
+        INPUT: 
+        RETURNS: Integer with number of components
+        '''
+        print('Super count returning {}'.format(self.N))
+        return self.N
+        
+    @classmethod
+    def create_from_file(cls, filename=None):
+        if filename is None:
+            filename = DEF_FILE
+        
+        line_count = 0
+        with open(filename, 'r') as f:
+            for line in f:
+                line_count += 1
+                
+                # Create new Union-Find using N on first line of file
+                if line_count == 1:
+                    N = int(line)
+                    print('Read init line to create {} sites'.format(N))
+                    new_class = cls(N)
+                else:
+                    # Read in the line, strip space and convert to integer 
+                    print('Read Line #{} with sites: {}'.format(line_count, line))
+                    sites = line.split(' ')
+                    sites = [int(site.strip()) for site in sites]
+                    p, q = sites
+                    print('Sites {} and {} connected? {}'.format(p, q, new_class.connected(p, q)))
+                    # if not new_class.connected(p, q):
+                    #     new_class.union(p, q)
+
+        print('File {}\nUnion-Find:\n{}'.format(filename, new_class))
+        return new_class
+
+class QuickFindUnionFind(BookUnionFind):
+    ''' Quick Find invariant:
+    `p` and `q` are connected iff id[p] == id[q].
+    All sites in the same component must have the same value in the id list
+    When creating union, change all values in id[p] to id[q]
+    '''
+    def __init__(self, filename=None):
+        '''
+        Initializes Union Find structure from file
+        '''
+        if filename is None:
+            filename = DEF_FILE
+        
+        line_count = 0
+        with open(filename, 'r') as f:
+            for line in f:
+                line_count += 1
+                
+                # Create new Union-Find using N on first line of file
+                if line_count == 1:
+                    self.N = int(line)
+                    self.id = list(range(self.N))
+                    print('Read init line to create {} sites'.format(self.N))
+                else:
+                    # Read in the line, strip space and convert to integer 
+                    print('Read Line #{} with sites: {}'.format(line_count, line))
+                    sites = line.split(' ')
+                    sites = [int(site.strip()) for site in sites]
+                    p, q = sites
+                    if not self.connected(p, q):
+                        self.union(p, q)
+
+    def union(self, p, q):
+        '''
+        Merge components if two sites are in different components 
+        INPUT: Site identifiers p and q
+        RETURNS: None
+        '''
+        print('Union of sites {} and {}'.format(p, q))
+        self.id = [self.id[q] if val == self.id[p] else val for idx, val in enumerate(self.id)]
+        print('self.id: {}'.format(self.id))
+        
+    def find(self, p):
+        '''
+        Component identifier for p
+          -> Implement this in subclass
+        INPUT: Site p
+        RETURNS: Component ID
+        '''
+        print('Find of site {} = {}'.format(p, self.id[p]))
+        # print(self.id)
+        return self.id[p]
+        
+    def connected(self, p, q):
+        '''
+        Returns True if p and q are in the same component
+        INPUT: Nodes p and q
+        RETURNS: Boolean showing if the nodes are connected
+        '''
+        print('Super connected for {} and {}'.format(p, q))
+        return self.find(p) == self.find(q)
+
+    def count(self):
+        '''
+        Returns number of components (not sites)
+        INPUT: 
+        RETURNS: Integer with number of components
+        '''
+        print('Super count returning {}'.format(self.N))
+        return self.N
+        
+    
+def test_quickfind():
+    quick_find = QuickFindUnionFind()
+    assert quick_find.id == [1,1,1,8,8,1,1,1,8,8]
+
 def test_naive_union_find(filename=None):
     
     if filename is None:
-        filename = DATA_DIR + '/tinyUF.txt'
-    
+        filename = DEF_FILE
     
     line_count = 0
     with open(filename, 'r') as f:
@@ -173,7 +300,7 @@ def test_naive_union_find(filename=None):
                 print(union_find)
 
     print(union_find)
-    
+
 def main(argv=None):
     '''
     Function called to run main script including unit tests
@@ -182,8 +309,10 @@ def main(argv=None):
         -1: Invalid input
          0: Script completed successfully
     '''
-    print(timeit.repeat(test_naive_union_find, number=1000))
+    # print(timeit.repeat(test_naive_union_find, number=1000))
 
+    test_quickfind()
+    
     return 0
         
 if __name__ == '__main__':
